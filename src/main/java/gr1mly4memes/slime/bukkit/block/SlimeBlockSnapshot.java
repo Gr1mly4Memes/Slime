696670df31ee45b86e4bc4fc29a2a21f10e078b5
Gr1mly4Memes <alagozantoni14@gmail.com>
2026-08-21 18:46:41 -0700

    fix some plugins cant break blocks

--- src/main/java/gr1mly4memes/slime/bukkit/block/SlimeBlockSnapshot.java
+++ src/main/java/gr1mly4memes/slime/bukkit/block/SlimeBlockSnapshot.java
@@ -3,17 +3,17 @@ package gr1mly4memes.slime.bukkit.block;
 import net.minecraft.world.level.block.state.BlockState;
 import org.bukkit.craftbukkit.block.CraftBlock;
 
-public class BlockSnapshot extends CraftBlock {
+public class SlimeBlockSnapshot extends CraftBlock {
 
     private final BlockState blockState;
 
-    public BlockSnapshot(net.neoforged.neoforge.common.util.BlockSnapshot blockSnapshot, boolean current) {
+    public SlimeBlockSnapshot(net.neoforged.neoforge.common.util.BlockSnapshot blockSnapshot, boolean current) {
         super(blockSnapshot.getLevel(), blockSnapshot.getPos());
         this.blockState = current ? blockSnapshot.getCurrentState() : blockSnapshot.getState();
     }
 
-    public static BlockSnapshot fromBlockSnapshot(net.neoforged.neoforge.common.util.BlockSnapshot blockSnapshot, boolean current) {
-        return new BlockSnapshot(blockSnapshot, current);
+    public static SlimeBlockSnapshot fromBlockSnapshot(net.neoforged.neoforge.common.util.BlockSnapshot blockSnapshot, boolean current) {
+        return new SlimeBlockSnapshot(blockSnapshot, current);
     }
 
     @Override
--- src/main/java/gr1mly4memes/slime/eventhandler/EventDispatcherRegistry.java
+++ src/main/java/gr1mly4memes/slime/eventhandler/EventDispatcherRegistry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) MohistMC.
+ * Copyright (C) Gr1mly4Memes.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,7 +17,8 @@
 
 package gr1mly4memes.slime.eventhandler;
 
-import gr1mly4memes.slime.eventhandler.dispatcher.*;
+import gr1mly4memes.slime.eventhandler.dispatcher.ItemEventDispatcher;
+import gr1mly4memes.slime.eventhandler.dispatcher.PlayerEventDispatcher;
 import net.neoforged.neoforge.common.NeoForge;
 
 public class EventDispatcherRegistry {
